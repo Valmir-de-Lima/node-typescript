b171aad2677b27e3de7b5e894d573799969ad253
Valmir de Lima <valmirblima@hotmail.com>
2024-04-08 20:40:35 -0300

    First commit

---
+++ src/backoffice/models/result.model.ts
@@ -0,0 +1,10 @@
+export class Result {
+    constructor(
+        public massege: string,
+        public success: boolean,
+        public data: any,
+        public error: any
+
+    ) {
+    }
+}
